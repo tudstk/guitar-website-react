e6dbcb956a727cfe8400467405944e04a757538d
Tudor Stroescu <tudorstroescu@yahoo.com>
2022-08-22 12:58:03 +0300

    fixed navbar

--- src/Components/navbar/Navbar.jsx
+++ src/Components/navbar/Navbar.jsx
@@ -8,15 +8,15 @@ import fbIcon from './fb.png';
 
 const Navbar = () => {
     return(
-        <div className='flex justify-between w-screen bg-navbar-black text-white sticky top-0 z-50'>
+        <div className='flex justify-between w-screen bg-navbar-black text-white fixed top-0 z-50'>
             {/* <p className='m-5 ml-6 md:ml-32'>
                 <h1 className="opacity-80">Guitar Shop</h1>
             </p> */}
             <ul className='flex flex-row justify-between content-center mr-2 md:ml-32'>
                 <li>
-                    <h1 className="opacity-100 text-red-300 m-5">MC MUSIC</h1>
+                    <h1 className="opacity-100 text-red-300 m-5">Logo</h1>
                 </li>
-                <li className="hidden md:block m-5 ml-28 opacity-70 transition duration-150 hover:opacity-100 cursor-pointer ease-in">HOME</li>
+                <li className="hidden md:block m-5 ml-20 opacity-70 transition duration-150 hover:opacity-100 cursor-pointer ease-in">HOME</li>
                 <li className="hidden md:block m-5 opacity-70 transition duration-150 hover:opacity-100 cursor-pointer ease-in">ABOUT</li>
                 <li className="hidden md:block m-5 opacity-70 transition duration-150 hover:opacity-100 cursor-pointer ease-in">CONTACT</li>
             </ul>
@@ -25,19 +25,19 @@ const Navbar = () => {
             <ul className='flex flex-row justify-between content-center mr-2 md:mr-32'>
                 
                 <li className="m-5">
-                    <img src={ igIcon } alt="igIcon" className="hidden md:block w-6 opacity-70 transition duration-150 hover:opacity-100 cursor-pointer ease-in"/>
+                    <img src={ igIcon } alt="igIcon" className="hidden md:block w-5 opacity-70 transition duration-150 hover:opacity-100 cursor-pointer ease-in"/>
                 </li>
                 <li className="m-5 ml-3">
-                    <img src={ fbIcon } alt="fbIcon" className="hidden md:block w-6 opacity-70 transition duration-150 hover:opacity-100 cursor-pointer ease-in"/>
+                    <img src={ fbIcon } alt="fbIcon" className="hidden md:block w-5 opacity-70 transition duration-150 hover:opacity-100 cursor-pointer ease-in"/>
                 </li>
-                <li className='m-5'>
-                    <Link to='/cart'>
-                        <img src={ shoppingCartIcon } alt="shoppingCartIcon" className="w-6 opacity-70 transition duration-150 hover:opacity-100 cursor-pointer ease-in"/>
+                <li className="m-5">
+                    <Link to="/cart">
+                        <img src={ shoppingCartIcon } alt="shoppingCartIcon" className="w-5 opacity-70 transition duration-150 hover:opacity-100 cursor-pointer ease-in"/>
                     </Link>
                 </li>
                 <li className='m-5'>
                     <Link to='/login'>
-                        <img src={ accountIcon } alt="accountIcon" className="w-6 opacity-70 transition duration-150 hover:opacity-100 cursor-pointer ease-in"/>
+                        <img src={ accountIcon } alt="accountIcon" className="w-5 opacity-70 transition duration-150 hover:opacity-100 cursor-pointer ease-in"/>
                     </Link>
                 </li>
             </ul>
