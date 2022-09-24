59948ca1b84d5591dba642f0b10bf90dc50cddd0
Tudor Stroescu <tudorstroescu@yahoo.com>
2022-09-25 02:20:06 +0300

    mobile search-bar-errorMsg bug fix

--- src/Components/search bar/SearchBar.tsx
+++ src/Components/search bar/SearchBar.tsx
@@ -37,15 +37,15 @@ function SearchBar({placeholder, data}:any) {
       <div className='flex flex-col'>
         <div className='flex'>
             <input placeholder={placeholder} 
-            className="bg-navbar-black-2 w-64 h-8 text-slate-200 opacity-80 p-4 pl-6 rounded-l-3xl focus:outline-0 focus:opacity-100"
+            className="bg-navbar-black-2 w-64 h-10 md:h-8 text-slate-200 opacity-80 p-4 pl-6 rounded-l-3xl focus:outline-0 focus:opacity-100"
             onChange={handleFilter}
             value={wordEntered}
             />
-            <div className='bg-navbar-black-2 h-8 rounded-r-3xl md:flex items-center justify-center p-1 md:p-2'>
+            <div className='bg-navbar-black-2 h-10 md:h-8 rounded-r-3xl md:flex items-center justify-center p-2'>
               { filteredData.length === 0 ? <SearchIcon className='opacity-80  text-slate-400'/> : <CloseIcon className='opacity-80 text-slate-400 hover:cursor-pointer hover:opacity-100' onClick={handleClose}/>}
             </div>
           </div>
-          {error === true ? <div className='absolute top left-2/4 top-11 bg-div-gray p-4 text-slate-400 mt-1'>No results found for your search</div> : ''}
+          {error === true ? <div className='md:absolute left-2/4 md:top-11 bg-div-gray p-4 text-slate-400 mt-1'>No results found for your search</div> : ''}
       </div>
       { filteredData.length !== 0 && (
         <div className='md:absolute top-12 w-64 md:w-80 h-24 bg-navbar-black text-slate-300 overflow-hidden flex flex-col text-start '>
